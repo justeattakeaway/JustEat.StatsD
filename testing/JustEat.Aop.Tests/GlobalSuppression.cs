e37aef943ded06e07bc57d1fa61a0023c11cbf27
peter.mounce <peter.mounce@d6dd0074-7a69-5544-9745-f2d122130f32>
2012-04-13 15:34:54 +0000

    .NET skeleton + tools
    git-svn-id: https://scm.dev:8443/svn/lib_statsd/trunk@5 d6dd0074-7a69-5544-9745-f2d122130f32

---
+++ testing/JustEat.Aop.Tests/GlobalSuppression.cs
@@ -0,0 +1,4 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames", Justification = "Unnecessary for internal code")]
+[assembly: SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic", Justification = "Tests project")]
