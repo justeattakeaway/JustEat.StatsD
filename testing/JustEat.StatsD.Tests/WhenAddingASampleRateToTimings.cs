896940a09e533dd1547b12f42a7b4c51108381e1
Emmanuel Acheampong <emmanuel.acheampong@just-eat.com>
2012-11-20 19:17:25 +0000

    Deleted a lot of stuff which isnt necessary for LibStatsd, add some test to try and get coverage, more work to do, will eventually add its own sending udp client into the fold.  Removed nlog as we dont need it here, this is for now just going to be used to format strings, and eventually send in a manner we want.

---
+++ testing/JustEat.StatsD.Tests/WhenAddingASampleRateToTimings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using JustEat.Testing;
+using Shouldly;
+
+namespace JustEat.StatsD.Tests
+{
+	public class WhenFormattingMetricsToTimings : BehaviourTest<StatsDMessageFormatter>
+	{
+		private string _someBucketName;
+		private long _someValueToSend;
+		private CultureInfo _someCulture;
+		private string _result;
+		private double _sampleRate;
+
+		protected override StatsDMessageFormatter CreateSystemUnderTest()
+		{
+			return new StatsDMessageFormatter();
+		}
+
+		protected override void Given()
+		{
+			var random = new Random();
+			_someBucketName = "counter-bucket";
+			_someValueToSend = random.Next(100);
+			_sampleRate = 0.9;
+			_someCulture = new CultureInfo("en-US");
+		}
+
+		protected override void When()
+		{
+			// Need to mock the results... so random isnt so random here...otherwise we get a test fail when comparing the formatted string...
+			_result = SystemUnderTest.Timing(_someValueToSend, _sampleRate, _someBucketName);
+		}
+
+
+		[Then]
+		public void FormattedStringShouldBeCorrectlyFormatted()
+		{
+			_result.ShouldBe(string.Format(_someCulture, "{0}:{1}|ms|@{2:f}\n", _someBucketName, _someValueToSend, _sampleRate));
+		}
+
+		[Then]
+		public void NoExceptionsShouldHaveBeenThrown()
+		{
+			ThrownException.ShouldBe(null);
+		}
+	}
+}
