e467f60657c1ffa3cc384b4908768c6c584d33d1
Peter Mounce <peter.mounce@just-eat.com>
2012-12-10 17:13:43 +0000

    Initial stab at a client
    * No tests yet - don't pull it until I've written some
    * Want to get feedback for whether you're ok with the direction of
    the change
      * Intention is to provide a simple entry-point for consumers to
      just set up and use, without needing to understand the innards of
      formatting, the spec, or UDP.
      * Later, would add a `Flush` method to the interface, and provide
      an implementation that would batch up the messages and send them
      asynchronously.

---
+++ src/JustEat.StatsD/StatsDImmediatePublisher.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Globalization;
+
+namespace JustEat.StatsD
+{
+	/// <summary>
+	/// Will synchronously publish stats at statsd as you make calls; will not batch sends.
+	/// </summary>
+	public class StatsDImmediatePublisher : IStatsDPublisher
+	{
+		private readonly StatsDMessageFormatter _formatter;
+		private readonly IStatsDUdpClient _transport;
+
+		public StatsDImmediatePublisher(CultureInfo cultureInfo, string hostNameOrAddress, int port)
+		{
+			_formatter = new StatsDMessageFormatter(cultureInfo);
+			_transport = new StatsDUdpClient(hostNameOrAddress, port);
+		}
+
+		public void Increment(string bucket)
+		{
+			_transport.Send(_formatter.Increment(bucket));
+		}
+
+		public void Increment(long value, string bucket)
+		{
+			_transport.Send(_formatter.Increment(value, bucket));
+		}
+
+		public void Increment(long value, double sampleRate, string bucket)
+		{
+			_transport.Send(_formatter.Increment(value, sampleRate, bucket));
+		}
+
+		public void Increment(long value, double sampleRate, params string[] buckets)
+		{
+			_transport.Send(_formatter.Increment(value, sampleRate, buckets));
+		}
+
+		public void Decrement(string bucket)
+		{
+			_transport.Send(_formatter.Decrement(bucket));
+		}
+
+		public void Decrement(long value, string bucket)
+		{
+			_transport.Send(_formatter.Decrement(value, bucket));
+		}
+
+		public void Decrement(long value, double sampleRate, string bucket)
+		{
+			_transport.Send(_formatter.Decrement(value, sampleRate, bucket));
+		}
+
+		public void Decrement(long value, double sampleRate, params string[] buckets)
+		{
+			_transport.Send(_formatter.Decrement(value, sampleRate, buckets));
+		}
+
+		public void Gauge(long value, string bucket)
+		{
+			_transport.Send(_formatter.Gauge(value, bucket));
+		}
+
+		public void Gauge(long value, string bucket, DateTime timeStamp)
+		{
+			_transport.Send(_formatter.Gauge(value, bucket, timeStamp));
+		}
+
+		public void Timing(TimeSpan duration, string bucket)
+		{
+			_transport.Send(_formatter.Timing(Convert.ToInt64(duration.TotalMilliseconds), bucket));
+		}
+
+		public void Timing(TimeSpan duration, double sampleRate, string bucket)
+		{
+			_transport.Send(_formatter.Timing(Convert.ToInt64(duration.TotalMilliseconds), sampleRate, bucket));
+		}
+	}
+
+	public interface IStatsDPublisher
+	{
+		void Increment(string bucket);
+		void Increment(long value, string bucket);
+		void Increment(long value, double sampleRate, string bucket);
+		void Increment(long value, double sampleRate, params string[] buckets);
+		void Decrement(string bucket);
+		void Decrement(long value, string bucket);
+		void Decrement(long value, double sampleRate, string bucket);
+		void Decrement(long value, double sampleRate, params string[] buckets);
+		void Gauge(long value, string bucket);
+		void Gauge(long value, string bucket, DateTime timeStamp);
+		void Timing(TimeSpan duration, string bucket);
+		void Timing(TimeSpan duration, double sampleRate, string bucket);
+	}
+}
