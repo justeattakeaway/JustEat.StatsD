363047a9c1f553248d47c75ca31e8d76e940806c
Peter Mounce <peter.mounce@just-eat.com>
2012-12-11 13:52:04 +0000

    Extract to file by itself.

--- src/JustEat.StatsD/StatsDImmediatePublisher.cs
+++ src/JustEat.StatsD/StatsDImmediatePublisher.cs
@@ -79,20 +79,4 @@ namespace JustEat.StatsD
 			_transport.Send(_formatter.Timing(Convert.ToInt64(duration.TotalMilliseconds), sampleRate, bucket));
 		}
 	}
-
-	public interface IStatsDPublisher
-	{
-		void Increment(string bucket);
-		void Increment(long value, string bucket);
-		void Increment(long value, double sampleRate, string bucket);
-		void Increment(long value, double sampleRate, params string[] buckets);
-		void Decrement(string bucket);
-		void Decrement(long value, string bucket);
-		void Decrement(long value, double sampleRate, string bucket);
-		void Decrement(long value, double sampleRate, params string[] buckets);
-		void Gauge(long value, string bucket);
-		void Gauge(long value, string bucket, DateTime timeStamp);
-		void Timing(TimeSpan duration, string bucket);
-		void Timing(TimeSpan duration, double sampleRate, string bucket);
-	}
 }
