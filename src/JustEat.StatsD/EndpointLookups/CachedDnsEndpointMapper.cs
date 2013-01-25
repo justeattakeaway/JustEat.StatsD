1ffd36cb89c6a345e4e5800f8bcc5d7daae6ea9d
Anton <anton.jefcoate@just-eat.com>
2013-01-25 14:57:02 +0000

    Added caching of resolved DNS IP as it was causing performance issues in heavily threaded apps (BoxHandler)

---
+++ src/JustEat.StatsD/EndpointLookups/CachedDnsEndpointMapper.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Net;
+
+namespace JustEat.StatsD.EndpointLookups
+{
+    /// <summary>
+    /// Looks up an endpoint and holds the ip endpoint in memory for the specified duration (seconds)
+    /// </summary>
+    public class CachedDnsEndpointMapper : IDnsEndpointMapper
+    {
+        private readonly IDnsEndpointMapper _baseMapper;
+        private readonly int _secondsCacheDuration;
+        private CachedEndPoint _cachedEndPoint;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="baseMapper">Base endpoint provider</param>
+        /// <param name="secondsCacheDuration">Seconds to cache ip endpoints for</param>
+        public CachedDnsEndpointMapper(IDnsEndpointMapper baseMapper, int secondsCacheDuration)
+        {
+            _baseMapper = baseMapper;
+            _secondsCacheDuration = secondsCacheDuration;
+        }
+
+        /// <summary>
+        /// Get an IP Endpoint for the DNS host name
+        /// </summary>
+        /// <param name="hostName"></param>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public IPEndPoint GetIpEndPoint(string hostName, int port)
+        {
+            lock (this)
+            {
+                if (_cachedEndPoint == null || _cachedEndPoint.IsExpired(_secondsCacheDuration))
+                {
+                    _cachedEndPoint = new CachedEndPoint(_baseMapper.GetIpEndPoint(hostName, port));
+                }
+            }
+            return _cachedEndPoint.IpEndPoint;
+        }
+
+        internal class CachedEndPoint
+        {
+            internal IPEndPoint IpEndPoint { get; private set; }
+            internal DateTime CreateTime { get; private set; }
+
+            public CachedEndPoint(IPEndPoint endpoint)
+            {
+                IpEndPoint = endpoint;
+                CreateTime = DateTime.UtcNow;
+            }
+
+            public bool IsExpired(int secondsCacheDuration)
+            {
+                return CreateTime.AddSeconds(secondsCacheDuration) <= DateTime.UtcNow;
+            }
+        }
+    }
+}
---
+++ testing/PerfTestHarness/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using JustEat.StatsD;
+
+namespace PerfTestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var iterations = Enumerable.Range(1, 500000);
+            var client = new StatsDUdpClient(10, "localhost", 3128);
+            var formatter = new StatsDMessageFormatter();
+            var watch = new Stopwatch();
+
+            Console.WriteLine("To start - hit ENTER.");
+            Console.ReadLine();
+            Console.WriteLine("start");
+            watch.Start();
+
+            Parallel.ForEach(iterations, x => client.Send(formatter.Gauge(x, "bucket_sample" + "number-of-messages-to-be-sent")));
+        
+            watch.Stop();
+            Console.WriteLine("end - " + watch.ElapsedMilliseconds);
+            Console.ReadLine();
+        }
+    }
+}
