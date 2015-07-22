bb2db45c7b9ac0fabade6e33624711af3fed75ba
Peter Mounce <peter.mounce@just-eat.com>
2015-07-22 15:44:45 +0100

    fix nuspec author token

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
 [assembly: AssemblyDescription("JUST EAT statsd metrics-publishing")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("JUST EAT")]
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
