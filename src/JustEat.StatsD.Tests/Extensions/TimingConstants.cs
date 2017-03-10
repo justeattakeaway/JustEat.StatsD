63fbfe9cfe493d5f87942faed7ab5d6a9bf8203b
Anthony Steele <anthony.steele@just-eat.com>
2017-03-10 15:08:22 +0000

    Very longer test timeouts :( :( :(

--- src/JustEat.StatsD.Tests/Extensions/TimingConstants.cs
+++ src/JustEat.StatsD.Tests/Extensions/TimingConstants.cs
@@ -4,9 +4,9 @@ namespace JustEat.StatsD.Extensions
 {
     public static class TimingConstants
     {
-        public const int DelayMilliseconds = 400;
+        public const int DelayMilliseconds = 500;
 
         public static readonly TimeSpan DeltaFast = TimeSpan.FromMilliseconds(DelayMilliseconds / 4);
-        public static readonly TimeSpan DeltaSlow = TimeSpan.FromMilliseconds(DelayMilliseconds);
+        public static readonly TimeSpan DeltaSlow = TimeSpan.FromMilliseconds(DelayMilliseconds * 2);
     }
 }
