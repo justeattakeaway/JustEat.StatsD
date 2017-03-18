df704b3b650aa1b18145c8a4ed7d9a0df7660407
AnthonySteele <Anthony@PlasticAvalon.org>
2017-03-18 10:05:09 +0000

    No point in having a requery interval for "localhost" - the IP is not going to change

--- src/PerfTestHarness/Program.cs
+++ src/PerfTestHarness/Program.cs
@@ -13,7 +13,7 @@ namespace PerfTestHarness
         private static void Main(string[] args)
         {
             var iterations = Enumerable.Range(1, 500000);
-            var endpoint = EndpointParser.MakeEndPointSource("localhost", 3128, TimeSpan.FromMinutes(1));
+            var endpoint = EndpointParser.MakeEndPointSource("localhost", 3128, null);
             var client = new StatsDUdpTransport(endpoint);
             var formatter = new StatsDMessageFormatter(CultureInfo.InvariantCulture);
             var watch = new Stopwatch();
