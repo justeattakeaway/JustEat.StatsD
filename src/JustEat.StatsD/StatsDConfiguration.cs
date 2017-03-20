75f3a808d6cc355b45c5c838a3b8599046d4eb7d
Anthony Steele <anthony.steele@just-eat.com>
2017-03-20 13:46:17 +0000

    docs for tooltips docs for tooltips on the StatsDConfiguration

--- src/JustEat.StatsD/StatsDConfiguration.cs
+++ src/JustEat.StatsD/StatsDConfiguration.cs
@@ -8,13 +8,35 @@ namespace JustEat.StatsD
         public const int DefaultPort = 8125;
         public static readonly TimeSpan DefaultDnsLookupInterval = TimeSpan.FromMinutes(5);
 
+        /// <summary>
+        /// The host name or IP address of the statsD server. 
+        /// This field must be set. 
+        /// </summary>
         public string Host { get; set; }
 
+        /// <summary>
+        /// The port on the satsD server that stas are sent to.
+        /// Default is the standard port (8125).
+        /// </summary>
         public int Port { get; set; } = DefaultPort;
 
+        /// <summary>
+        /// Length of time to cache the host name to Ip address lookup.
+        /// Only used when "Host" contains a host name.
+        /// Default is 5 minutes.
+        /// </summary>
         public TimeSpan? DnsLookupInterval { get; set; } = DefaultDnsLookupInterval;
 
+        /// <summary>
+        /// Prepend a prefix to all stats.
+        /// Default is empty.
+        /// </summary>
         public string Prefix { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Culture for formatting stats.
+        /// Default is InvariantCulture.
+        /// </summary>
         public CultureInfo Culture { get; set; } = CultureInfo.InvariantCulture;
     }
 }
