122b7d0632ac09988314a94015c5bf60196bed48
martincostello <martin@martincostello.com>
2018-07-18 09:23:54 +0100

    Pool sockets in UdpTransport
    Add a pooling implementation detail to UdpTransport.

--- src/JustEat.StatsD/UdpTransport.cs
+++ src/JustEat.StatsD/UdpTransport.cs
@@ -8,15 +8,50 @@ using JustEat.StatsD.EndpointLookups;
 
 namespace JustEat.StatsD
 {
-    public class UdpTransport : IStatsDTransport
+    /// <summary>
+    /// A class representing an implementation of <see cref="IStatsDTransport"/> uses UDP.
+    /// </summary>
+    public class UdpTransport : IStatsDTransport, IDisposable
     {
-        private readonly IPEndPointSource _endpointSource;
+        private const int PoolSize = 30;
 
+        private static readonly SimpleObjectPool<SocketAsyncEventArgs> EventArgsPool
+            = new SimpleObjectPool<SocketAsyncEventArgs>(PoolSize, pool => new PoolAwareSocketAsyncEventArgs(pool));
+
+        private readonly SimpleObjectPool<Socket> _socketPool
+            = new SimpleObjectPool<Socket>(PoolSize, pool => CreateSocket());
+
+        private readonly IPEndPointSource _endpointSource;
+        private bool _disposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UdpTransport"/> class.
+        /// </summary>
+        /// <param name="endPointSource">The <see cref="IPEndPointSource"/> to use.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="endPointSource"/> is <see langword="null"/>.
+        /// </exception>
         public UdpTransport(IPEndPointSource endPointSource)
         {
             _endpointSource = endPointSource ?? throw new ArgumentNullException(nameof(endPointSource));
         }
 
+        /// <summary>
+        /// Finalizes an instance of the <see cref="UdpTransport"/> class.
+        /// </summary>
+        ~UdpTransport()
+        {
+            Dispose(false);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <inheritdoc />
         public void Send(string metric)
         {
             if (string.IsNullOrWhiteSpace(metric))
@@ -30,6 +65,28 @@ namespace JustEat.StatsD
             using (var socket = CreateSocket())
             {
                 socket.SendTo(bytes, endpoint);
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <see langword="true" /> to release both managed and unmanaged resources;
+        /// <see langword="false" /> to release only unmanaged resources.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                Socket socket = null;
+
+                while ((socket = _socketPool.Pop()) != null)
+                {
+                    socket.Dispose();
+                }
+
+                _disposed = true;
             }
         }
 
