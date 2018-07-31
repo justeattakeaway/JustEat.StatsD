ffff9c8ded5e3c506d2b2734b1f4e59643c0df39
Anthony Steele <anthony.steele@just-eat.com>
2018-07-31 14:11:23 +0100

    $env:

--- SetAppVeyorBuildVersion.ps1
+++ SetAppVeyorBuildVersion.ps1
@@ -27,9 +27,9 @@ if ($env:APPVEYOR_REPO_TAG -ne "true") {
   }
 }
 else {
-  if ($APPVEYOR_REPO_TAG_NAME.Contains("-")) {
-    $dashIndex = $APPVEYOR_REPO_TAG_NAME.IndexOf("-")
-    $versionSuffix = $APPVEYOR_REPO_TAG_NAME.Substring($dashIndex + 1)
+  if ($env:APPVEYOR_REPO_TAG_NAME.Contains("-")) {
+    $dashIndex = $env:APPVEYOR_REPO_TAG_NAME.IndexOf("-")
+    $versionSuffix = $env:APPVEYOR_REPO_TAG_NAME.Substring($dashIndex + 1)
   }
 
 }
