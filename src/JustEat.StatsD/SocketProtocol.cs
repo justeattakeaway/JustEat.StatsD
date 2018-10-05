ace0beb7bfc89e1b2efca10b2bdd179f7df49fc7
Anthony Steele <anthony.steele@just-eat.com>
2018-10-05 09:40:15 +0100

    moar docs

--- src/JustEat.StatsD/SocketProtocol.cs
+++ src/JustEat.StatsD/SocketProtocol.cs
@@ -2,11 +2,19 @@ namespace JustEat.StatsD
 {
     /// <summary>
     /// The subset of ProtocolType that are supported by SocketTransport
-    /// UDP or IP
+    /// UDP or IP.
+    /// UDP is the default, but IP transport is required for AWS Lambdas.
     /// </summary>
     public enum SocketProtocol
     {
+        /// <summary>
+        /// transport stats over UDP sockets
+        /// </summary>
         Udp,
+
+        /// <summary>
+        /// transport stats over IP sockets
+        /// </summary>
         IP
     }
 }
