1d7c9f024bd10eceb440629da7da016982e70f32
martincostello <martin@martincostello.com>
2018-10-07 12:32:16 +0100

    Add XML documentation
    Add XML documentation to StatsDPublisher.

--- src/JustEat.StatsD/StatsDPublisher.cs
+++ src/JustEat.StatsD/StatsDPublisher.cs
@@ -5,8 +5,12 @@ using JustEat.StatsD.EndpointLookups;
 namespace JustEat.StatsD
 {
     /// <summary>
-    ///     Will synchronously publish stats at statsd as you make calls; will not batch sends.
+    /// A class representing the default implementation of <see cref="IStatsDPublisher"/> that
+    /// publishes counters, gauges and timers to statsD. This class cannot be inherited.
     /// </summary>
+    /// <remarks>
+    /// Metrics are published synchronously immediately and are not batched.
+    /// </remarks>
     public sealed class StatsDPublisher : IStatsDPublisher, IDisposable
     {
         private readonly IStatsDPublisher _inner;
@@ -15,6 +19,17 @@ namespace JustEat.StatsD
         private bool _disposed;
         private bool _disposeTransport;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StatsDPublisher"/> class for the specified transport.
+        /// </summary>
+        /// <param name="configuration">The statsD configuration to use.</param>
+        /// <param name="transport">The transport implementation to use.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="configuration"/> or <paramref name="transport"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="configuration"/> is invalid, such as an invalid hostname or IP address.
+        /// </exception>
         public StatsDPublisher(StatsDConfiguration configuration, IStatsDTransport transport)
         {
             if (configuration == null)
@@ -41,6 +56,16 @@ namespace JustEat.StatsD
             _disposeTransport = false;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StatsDPublisher"/> class using the default transport.
+        /// </summary>
+        /// <param name="configuration">The statsD configuration to use.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="configuration"/> is <see langword="null"/>.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="configuration"/> is invalid, such as an invalid hostname or IP address.
+        /// </exception>
         public StatsDPublisher(StatsDConfiguration configuration)
         {
             if (configuration == null)
@@ -66,81 +91,97 @@ namespace JustEat.StatsD
             }
         }
 
+        /// <inheritdoc />
         public void MarkEvent(string name)
         {
             _inner.MarkEvent(name);
         }
 
+        /// <inheritdoc />
         public void Increment(string bucket)
         {
             _inner.Increment(bucket);
         }
 
+        /// <inheritdoc />
         public void Increment(long value, string bucket)
         {
             _inner.Increment(value, bucket);
         }
 
+        /// <inheritdoc />
         public void Increment(long value, double sampleRate, string bucket)
         {
             _inner.Increment(value, sampleRate, bucket);
         }
 
+        /// <inheritdoc />
         public void Increment(long value, double sampleRate, params string[] buckets)
         {
             _inner.Increment(value, sampleRate, buckets);
         }
 
+        /// <inheritdoc />
         public void Decrement(string bucket)
         {
             _inner.Decrement(bucket);
         }
 
+        /// <inheritdoc />
         public void Decrement(long value, string bucket)
         {
             _inner.Decrement(value, bucket);
         }
 
+        /// <inheritdoc />
         public void Decrement(long value, double sampleRate, string bucket)
         {
             _inner.Decrement(value, sampleRate, bucket);
         }
 
+        /// <inheritdoc />
         public void Decrement(long value, double sampleRate, params string[] buckets)
         {
             _inner.Decrement(value, sampleRate, buckets);
         }
 
+        /// <inheritdoc />
         public void Gauge(double value, string bucket)
         {
             _inner.Gauge(value, bucket);
         }
 
+        /// <inheritdoc />
         public void Gauge(long value, string bucket)
         {
             _inner.Gauge(value, bucket);
         }
 
+        /// <inheritdoc />
         public void Timing(TimeSpan duration, string bucket)
         {
             _inner.Timing(duration, bucket);
         }
 
+        /// <inheritdoc />
         public void Timing(TimeSpan duration, double sampleRate, string bucket)
         {
             _inner.Timing(duration, sampleRate, bucket);
         }
 
+        /// <inheritdoc />
         public void Timing(long duration, string bucket)
         {
             _inner.Timing(duration, bucket);
         }
 
+        /// <inheritdoc />
         public void Timing(long duration, double sampleRate, string bucket)
         {
             _inner.Timing(duration, sampleRate, bucket);
         }
 
+        /// <inheritdoc />
         public void Dispose()
         {
             if (!_disposed)
