700527b30220031559489d5f51b17256f74957d2
martincostello <martin@martincostello.com>
2019-02-17 17:01:01 +0000

    Remove unused code
    Remove code that was only exercised by tests and benchmarks and wasn't otherwise used.

--- tests/Benchmark/Utf8FormatterBenchmark.cs
+++ tests/Benchmark/Utf8FormatterBenchmark.cs
@@ -1,6 +1,4 @@
-using System.Text;
 using BenchmarkDotNet.Attributes;
-using JustEat.StatsD;
 using JustEat.StatsD.Buffered;
 
 namespace Benchmark
@@ -8,18 +6,9 @@ namespace Benchmark
     [MemoryDiagnoser]
     public class Utf8FormatterBenchmark
     {
-        private static readonly StatsDMessageFormatter FormatterString = new StatsDMessageFormatter("hello.world");
         private static readonly StatsDUtf8Formatter FormatterBuffer = new StatsDUtf8Formatter("hello.world");
 
         private static readonly byte[] Buffer = new byte[512];
-
-        [Benchmark(Baseline = true)]
-        public void StringBased()
-        {
-            Encoding.UTF8.GetBytes(FormatterString.Gauge(255, "some.neat.bucket"));
-            Encoding.UTF8.GetBytes(FormatterString.Timing(255, "some.neat.bucket"));
-            Encoding.UTF8.GetBytes(FormatterString.Increment(255, "some.neat.bucket"));
-        }
 
         [Benchmark]
         public void BufferBased()
