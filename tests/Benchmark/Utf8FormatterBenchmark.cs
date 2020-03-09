f38c31dbb9886a249505585e20523a74ac4efd44
Carlos Henrique Guardao Gandarez <gandarez@gmail.com>
2020-03-09 12:06:19 -0400

    add benchmark tests when tags are present

--- tests/Benchmark/Utf8FormatterBenchmark.cs
+++ tests/Benchmark/Utf8FormatterBenchmark.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
 using JustEat.StatsD.Buffered;
 
@@ -13,9 +14,16 @@ namespace Benchmark
         [Benchmark]
         public void BufferBased()
         {
+            Dictionary<string, string> tags = new Dictionary<string, string>();
+            tags.Add("key", " value");
+            tags.Add("key2", " value2");
+
             FormatterBuffer.TryFormat(StatsDMessage.Gauge(255, "some.neat.bucket", null), 1, Buffer, out _);
             FormatterBuffer.TryFormat(StatsDMessage.Timing(255, "some.neat.bucket", null), 1, Buffer, out _);
             FormatterBuffer.TryFormat(StatsDMessage.Counter(255, "some.neat.bucket", null), 1, Buffer, out _);
+            FormatterBuffer.TryFormat(StatsDMessage.Gauge(255, "some.neat.bucket", tags), 1, Buffer, out _);
+            FormatterBuffer.TryFormat(StatsDMessage.Timing(255, "some.neat.bucket", tags), 1, Buffer, out _);
+            FormatterBuffer.TryFormat(StatsDMessage.Counter(255, "some.neat.bucket", tags), 1, Buffer, out _);
         }
     }
 }
